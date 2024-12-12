86b5571f75bcebcec5ad4fc7bf109ff5862bab9e
Lu Wang <rebmit@rebmit.moe>
2024-12-12 19:26:20 +0800

    users/root: add password

--- nixos/profiles/users/root/default.nix
+++ nixos/profiles/users/root/default.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ config, ... }:
 {
   users.users.root = {
+    hashedPasswordFile = config.sops.secrets."user-password/root".path;
     openssh.authorizedKeys.keyFiles = [
       ./_ssh/marisa-7d76
       ./_ssh/marisa-a7s
     ];
+  };
+
+  sops.secrets."user-password/root" = {
+    neededForUsers = true;
+    sopsFile = config.sops.secretFiles.get "common.yaml";
   };
 }
