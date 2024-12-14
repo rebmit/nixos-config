44cf0004f41ddd07d7736f49f83f1aa5b54e0935
Lu Wang <rebmit@rebmit.moe>
2024-12-14 21:10:44 +0800

    services/caddy: init

---
+++ nixos/profiles/services/caddy/default.nix
@@ -0,0 +1,22 @@
+{ config, mylib, ... }:
+{
+  services.caddy = {
+    enable = true;
+    enableReload = true;
+  };
+
+  systemd.services.caddy.serviceConfig = mylib.misc.serviceHardened // {
+    AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+    CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+  };
+
+  systemd.services.caddy-api.serviceConfig = mylib.misc.serviceHardened // {
+    AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+    CapabilityBoundingSet = [ "CAP_NET_BIND_SERVICE" ];
+  };
+
+  services.restic.backups.b2.paths = [
+    config.services.caddy.logDir
+    config.services.caddy.dataDir
+  ];
+}
