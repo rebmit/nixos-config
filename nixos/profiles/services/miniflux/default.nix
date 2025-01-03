370324be1c93c7c3eedf5bc7450667b4d6b5b6f8
Lu Wang <rebmit@rebmit.moe>
2025-01-03 22:41:37 +0800

    services/miniflux: enable client authentication

--- nixos/profiles/services/miniflux/default.nix
+++ nixos/profiles/services/miniflux/default.nix
@@ -1,5 +1,15 @@
 { config, ... }:
 {
+  sops.secrets."miniflux/oidc-client-secret" = {
+    sopsFile = config.sops.secretFiles.host;
+  };
+
+  systemd.services.miniflux.serviceConfig = {
+    LoadCredential = [
+      "oidc-client-secret:${config.sops.secrets."miniflux/oidc-client-secret".path}"
+    ];
+  };
+
   services.miniflux = {
     enable = true;
     config = rec {
@@ -8,6 +18,7 @@
       CREATE_ADMIN = 0;
       OAUTH2_PROVIDER = "oidc";
       OAUTH2_CLIENT_ID = "miniflux";
+      OAUTH2_CLIENT_SECRET_FILE = "/run/credentials/miniflux.service/oidc-client-secret";
       OAUTH2_REDIRECT_URL = "${BASE_URL}/oauth2/oidc/callback";
       OAUTH2_OIDC_PROVIDER_NAME = "id.rebmit.moe";
       OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://id.rebmit.moe/realms/rebmit";
