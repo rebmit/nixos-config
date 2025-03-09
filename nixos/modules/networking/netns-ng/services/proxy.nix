ce8f746df39886efb19d2bdc0b6342cc387217ca
Lu Wang <rebmit@rebmit.moe>
2025-03-09 20:17:09 +0800

    networking/netns-ng: add proxy integration

---
+++ nixos/modules/networking/netns-ng/services/proxy.nix
@@ -0,0 +1,90 @@
+{
+  config,
+  lib,
+  pkgs,
+  mylib,
+  ...
+}:
+let
+  inherit (lib) types;
+  inherit (lib.options) mkOption mkEnableOption;
+  inherit (lib.modules) mkMerge mkIf;
+  inherit (lib.attrsets) mapAttrs' nameValuePair;
+  inherit (lib.strings) concatMapStringsSep;
+  inherit (lib.meta) getExe;
+
+  inboundOptions =
+    { ... }:
+    {
+      options = {
+        netnsPath = mkOption {
+          type = types.str;
+          description = ''
+            Path to the inbound network namespace.
+          '';
+        };
+        listenPort = mkOption {
+          type = types.int;
+          description = ''
+            Port number for incoming connections.
+          '';
+        };
+      };
+    };
+in
+{
+  options.networking.netns-ng = mkOption {
+    type = types.attrsOf (
+      types.submodule (
+        { ... }:
+        {
+          options.services.proxy = {
+            enable = mkEnableOption "mixed proxy for other network namespaces";
+            inbounds = mkOption {
+              type = types.listOf (types.submodule inboundOptions);
+              default = [ ];
+              description = ''
+                List of inbound configurations for the proxy.
+              '';
+            };
+          };
+        }
+      )
+    );
+  };
+
+  config = {
+    systemd.services = mapAttrs' (
+      name: cfg:
+      nameValuePair "netns-${name}-proxy" (
+        mkIf (cfg.enable && cfg.services.proxy.enable) (mkMerge [
+          cfg.config
+          {
+            serviceConfig = mylib.misc.serviceHardened // {
+              Type = "simple";
+              Restart = "on-failure";
+              RestartSec = 5;
+              DynamicUser = true;
+              ExecStart = "${getExe pkgs.gost} ${
+                concatMapStringsSep " " (
+                  inbound: ''-L "auto://[::1]:${toString inbound.listenPort}?netns=${inbound.netnsPath}"''
+                ) cfg.services.proxy.inbounds
+              }";
+              ProtectProc = false;
+              RestrictNamespaces = "net";
+              AmbientCapabilities = [
+                "CAP_SYS_ADMIN"
+                "CAP_SYS_PTRACE"
+              ];
+              CapabilityBoundingSet = [
+                "CAP_SYS_ADMIN"
+                "CAP_SYS_PTRACE"
+              ];
+              SystemCallFilter = [ "@system-service" ];
+            };
+          }
+        ])
+      )
+    ) config.networking.netns-ng;
+  };
+}
