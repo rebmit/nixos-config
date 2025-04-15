4bb15497c864c6d3d28109365781bda4417945ba
Lu Wang <rebmit@rebmit.moe>
2025-04-15 13:54:24 +0800

    treewide: move prometheus, ntfy and vaultwarden to kanako-ham1

--- nixos/hosts/kanako-ham0/default.nix
+++ nixos/hosts/kanako-ham0/default.nix
@@ -8,9 +8,6 @@
   imports = [
     suites.server
     profiles.services.caddy
-    profiles.services.ntfy
-    profiles.services.prometheus.server
-    profiles.services.vaultwarden
   ] ++ (mylib.path.scanPaths ./. "default.nix");
 
   system.stateVersion = "24.11";
