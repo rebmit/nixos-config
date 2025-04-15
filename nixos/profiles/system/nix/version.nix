9d2164a7f75b1fa6350cb1afe355c4988b8e8466
Lu Wang <rebmit@rebmit.moe>
2025-04-15 13:23:06 +0800

    system/nix: use `nixVersions.nix_2_24` for now

--- nixos/profiles/system/nix/version.nix
+++ nixos/profiles/system/nix/version.nix
@@ -3,5 +3,7 @@ let
   inherit (lib.modules) mkDefault;
 in
 {
-  nix.package = mkDefault pkgs.nixVersions.stable;
+  # TODO: wait for the related issues to be resolved
+  # https://github.com/NixOS/nix/issues/13000
+  nix.package = mkDefault pkgs.nixVersions.nix_2_24;
 }
