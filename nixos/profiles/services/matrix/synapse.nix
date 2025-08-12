842fbd592ad555d1e513cd0b16cb0d754ba34049
Lu Wang <rebmit@rebmit.moe>
2025-08-12 23:44:19 +0800

    services/matrix: serve element-web for now

--- nixos/profiles/services/matrix/synapse.nix
+++ nixos/profiles/services/matrix/synapse.nix
@@ -1,7 +1,7 @@
 # Portions of this file are sourced from
 # https://github.com/NickCao/flakes/blob/3b03efb676ea602575c916b2b8bc9d9cd13b0d85/nixos/hcloud/hio0/matrix.nix (MIT License)
 # https://github.com/linyinfeng/dotfiles/blob/b618b0fd16fb9c79ab7199ed51c4c0f98a392cea/nixos/profiles/services/matrix/default.nix (MIT License)
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   services.matrix-synapse = {
     enable = true;
@@ -71,6 +71,28 @@
     extraConfig = ''
       reverse_proxy /_matrix/* 127.0.0.1:${toString config.ports.matrix-synapse}
       reverse_proxy /_synapse/* 127.0.0.1:${toString config.ports.matrix-synapse}
+
+      header {
+        X-Frame-Options SAMEORIGIN
+        X-Content-Type-Options nosniff
+        X-XSS-Protection "1; mode=block"
+        Content-Security-Policy "frame-ancestors 'self'"
+      }
+
+      file_server
+      root * "${
+        pkgs.element-web.override {
+          conf = {
+            default_server_config = {
+              "m.homeserver" = {
+                base_url = config.services.matrix-synapse.settings.public_baseurl;
+                server_name = config.services.matrix-synapse.settings.server_name;
+              };
+            };
+            show_labs_settings = true;
+          };
+        }
+      }"
     '';
   };
 
