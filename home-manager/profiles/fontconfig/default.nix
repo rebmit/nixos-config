439bc62c3c6ce5b98db0df42e86e70e73a46fede
Lu Wang <rebmit@rebmit.moe>
2025-09-26 16:12:28 +0800

    hm/fontconfig: add workaround for glycin bwrap issue with font symlinks

--- home-manager/profiles/fontconfig/default.nix
+++ home-manager/profiles/fontconfig/default.nix
@@ -1,15 +1,32 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
+let
+  inherit (lib.strings) concatMapStringsSep;
+in
 {
-  home.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-emoji
-    roboto-mono
-    nerd-fonts.roboto-mono
-  ];
+  fonts.fontconfig.enable = lib.mkForce false;
 
-  fonts.fontconfig.enable = true;
-
+  xdg.configFile."fontconfig/conf.d/10-hm-fonts.conf".text =
+    let
+      fonts = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-emoji
+        roboto-mono
+        nerd-fonts.roboto-mono
+      ];
+      cache = pkgs.makeFontsCache {
+        inherit (pkgs) fontconfig;
+        fontDirectories = fonts;
+      };
+    in
+    ''
+      <?xml version='1.0'?>
+      <!DOCTYPE fontconfig SYSTEM 'urn:fontconfig:fonts.dtd'>
+      <fontconfig>
+        ${concatMapStringsSep "\n" (font: "<dir>${font}</dir>") fonts}
+        <cachedir>${cache}</cachedir>
+      </fontconfig>
+    '';
   xdg.configFile."fontconfig/conf.d/30-default-fonts.conf".source = ./fonts.conf;
 }
